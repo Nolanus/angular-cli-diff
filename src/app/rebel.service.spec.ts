4846f9b2a510f6da8bce18249bce36e2638d32c2
Bot <3458616+Nolanus@users.noreply.github.com>
2017-11-09 16:05:40 +0200

    Service generation

---
+++ src/app/rebel.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { RebelService } from './rebel.service';
+
+describe('RebelService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [RebelService]
+    });
+  });
+
+  it('should ...', inject([RebelService], (service: RebelService) => {
+    expect(service).toBeTruthy();
+  }));
+});
