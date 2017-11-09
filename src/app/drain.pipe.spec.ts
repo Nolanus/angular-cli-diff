f0bcf4966af803f3c41d3466689687fc024afca5
Bot <3458616+Nolanus@users.noreply.github.com>
2017-11-09 15:43:06 +0200

    Pipe generation

---
+++ src/app/drain.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { DrainPipe } from './drain.pipe';
+
+describe('DrainPipe', () => {
+  it('create an instance', () => {
+    const pipe = new DrainPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
