3db535c47e675ad46aed0d711f90faa114e29e39
Bot <3458616+Nolanus@users.noreply.github.com>
2017-11-15 14:46:48 +0200

    Pipe generation

---
+++ src/app/drain.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DrainPipe } from './drain.pipe';
+
+describe('DrainPipe', () => {
+  it('create an instance', () => {
+    const pipe = new DrainPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
