ee77ad5b1dde2fb985ee9f0cafa0027038b276f1
Bot <3458616+Nolanus@users.noreply.github.com>
2017-11-23 07:47:32 +0000

    Pipe generation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DrainPipe } from './drain.pipe';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DrainPipe
   ],
   imports: [
     BrowserModule
