3ab54adcc5c71a70315f4c1700780fb681e8092d
Bot <3458616+Nolanus@users.noreply.github.com>
2017-11-30 07:52:32 +0000

    Directive generation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FooDirective } from './foo.directive';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooDirective
   ],
   imports: [
     BrowserModule
